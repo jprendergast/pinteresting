09cf86e4b56e380ec40810c80aba46872d0e6b41
John Prendergast <john@blueleaf.com>
2014-08-16 10:10:35 -0400

    fix production link ref

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Required for Heroku
-  config.action_mailer.default_url_options = { host: => 'myomrpinteresting.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'myomrpinteresting.herokuapp.com' }
 end
