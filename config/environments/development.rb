af1db61f0d337d058c8fbc2f90cf436f57746046
John Prendergast <john@blueleaf.com>
2014-08-19 18:21:51 -0400

    heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,8 @@ Rails.application.configure do
 
   #Required for Heroku
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #Required to make Imagemagick work
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
 end
